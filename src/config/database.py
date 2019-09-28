4f45bf23a322b3eb63cb77b6770a679c67d4e4ae
Keda87 <adiyatmubarak@gmail.com>
2019-09-28 13:08:50 +0700

    [FIX] Singleton for connection pool

--- src/config/database.py
+++ src/config/database.py
@@ -11,18 +11,16 @@ DB_DSN = f'postgres://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 
 class ConnectionPool:
-    _POOL = None
+   _POOL = None
 
-    class Pool:
-        @staticmethod
-        async def get_instance():
-            return await asyncpg.create_pool(DB_DSN, command_timeout=60)
-    
-    @classmethod
-    async def get_pool(cls):
-        if cls._POOL is not None:
-            return await cls._POOL
-        return await cls.Pool.get_instance()
+   def __init__(self):
+      if ConnectionPool._POOL != None:
+         raise Exception("This class is a singleton!")
+      else:
+         ConnectionPool._POOL = self
 
-
-DB_POOL = ConnectionPool.get_pool()
+   @classmethod
+   async def get_pool(cls):
+      if cls._POOL == None:
+         cls._POOL = await asyncpg.create_pool(DB_DSN, command_timeout=60)
+      return cls._POOL
